a727992b5bc81d8713abfd4ada91786daa4c8d96
majxd9 <hamamajd43@gmail.com>
2026-09-13 06:42:29 +0300

    fix: use correct v3 stylesheet path for exam

--- client/src/pages/Exam.tsx
+++ client/src/pages/Exam.tsx
@@ -4,7 +4,7 @@ import { api } from '../api/client';
 import { Question } from '../types';
 import OptimizedImage from '../components/OptimizedImage';
 import DiagramRenderer from '../components/DiagramRenderer';
-import './exam-v3.css';
+import '../login-v3.css';
 
 const DURATION = 15 * 60;
 
