b32fa259fa1caa98423d02059421163c38133e1b
majxd9 <hamamajd43@gmail.com>
2026-09-14 02:26:18 +0300

    ui: enable final responsive styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
 import './unified.css';
+import './modern-overrides.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><BrowserRouter><AuthProvider><App /></AuthProvider></BrowserRouter></React.StrictMode>
