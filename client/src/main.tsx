e6e6df3f8e2c92ecd2e445b9cadb85c6b06c77aa
majxd9 <hamamajd43@gmail.com>
2026-09-14 03:07:00 +0300

    Load final responsive mobile fixes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,7 @@ import './index.css';
 import './unified.css';
 import './modern-overrides.css';
 import './final-ui.css';
+import './mobile-fixes.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><BrowserRouter><AuthProvider><App /></AuthProvider></BrowserRouter></React.StrictMode>
