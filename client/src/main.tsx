bf7060a5a53ca2ab8835e00d58544caa6eabce5a
majxd9 <hamamajd43@gmail.com>
2026-09-17 20:18:18 +0300

    chore: load spirit v16 visual pass

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -27,6 +27,7 @@ import './spirit-v12.css';
 import './spirit-v13.css';
 import './spirit-v14.css';
 import './spirit-v15.css';
+import './spirit-v16.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
