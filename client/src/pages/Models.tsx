4af71c1d8053fe63e00fe30562d8b2d8b0af4075
majxd9 <hamamajd43@gmail.com>
2026-09-18 06:26:37 +0300

    fix(models): import React state hook from correct package

--- client/src/pages/Models.tsx
+++ client/src/pages/Models.tsx
@@ -1,4 +1,5 @@
-import { useNavigate, useState } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import SpiritDriveScene from '../components/SpiritDriveScene';
 import SpiritDrift from '../components/SpiritDrift';
 
